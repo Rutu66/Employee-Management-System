c04839225d1c447a0b62b5d3f9274f8bd32ff049
Rutu <rutukotadiya999@gmail.com>
2023-12-14 00:42:10 +0530

    final

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_emp/', views.add_emp, name='add_emp'),
+    path('remove_emp/<int:emp_id>/', views.remove_emp, name='remove_emp'),
+    
+    path('update_emp/<int:emp_id>/', views.update_emp, name='update_emp')
+    
+    
     
 ]
